8689d2cb1e98c8309b4427c40596ef177f57abdd
Star-caorui <Star_caorui@hotmail.com>
2021-11-23 08:23:18 +0800

    Fix some logic issues on the script.

--- shell/auto-conf-vps2arch.sh
+++ shell/auto-conf-vps2arch.sh
@@ -15,11 +15,15 @@ pacman -Rsn linux
 pacman -S linux-zen
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# Configure pacman mirror list.
+# Configure pacman archlinux mirror list.
 echo '#Server = https://cloudflaremirrors.com/archlinux/$repo/os/$arch
 Server = https://mirrors.bfsu.edu.cn/archlinux/$repo/os/$arch
 Server = https://mirrors.neusoft.edu.cn/archlinux/$repo/os/$arch
 Server = https://mirrors.tuna.tsinghua.edu.cn/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
+
+# Configure pacman archlinuxcn mirror list.
+echo 'Server = https://mirrors.bfsu.edu.cn/archlinuxcn/$arch
+Server = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/$arch' > /etc/pacman.d/mirrorlist-cn
 
 # Configure pacman config file.
 sed -e 's/#UseSyslog/UseSyslog/' /etc/pacman.conf
@@ -28,8 +32,6 @@ sed -e 's/#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf
 sed -e 's/#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 echo -e '\n[archlinuxcn]' >> /etc/pacman.conf
 echo 'Include = /etc/pacman.d/mirrorlist-cn' >> /etc/pacman.conf
-echo 'Server = https://mirrors.bfsu.edu.cn/archlinuxcn/$arch
-Server = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/$arch' > /etc/pacman.d/mirrorlist-cn
 echo -e '\n[web-worker]' >> /etc/pacman.conf
 echo 'Server = https://pkg.web-worker.cn/archlinux' >> /etc/pacman.conf
 echo -e '\n[zhullyb]' >> /etc/pacman.conf
@@ -40,27 +42,9 @@ pacman -S archlinuxcn-keyring web-worker-keyring zhullyb-keyring
 pacman -S oh-my-zsh-git zsh-autosuggestions zsh-syntax-highlighting
 ln -sf /usr/share/zsh/plugins/zsh-autosuggestions/ /usr/share/oh-my-zsh/plugins/
 ln -sf /usr/share/zsh/plugins/zsh-syntax-highlighting/ /usr/share/oh-my-zsh/plugins/
-echo 'XDG_CONFIG_HOME DEFAULT=@{HOME}/.config
-XDG_CACHE_HOME  DEFAULT=@{HOME}/.cache
-XDG_DATA_HOME   DEFAULT=@{HOME}/.local/share
-XDG_STATE_HOME  DEFAULT=@{HOME}/.local/state
-XDG_DATA_DIRS   DEFAULT=/usr/local/share:/usr/share
-XDG_CONFIG_DIRS DEFAULT=/etc/xdg
-
-GNUPGHOME       DEFAULT=${XDG_DATA_HOME}/gnupg
-VSCODE_PORTABLE DEFAULT=${XDG_DATA_HOME}/vscode
-_JAVA_OPTIONS   DEFAULT=-Djava.util.prefs.userRoot=${XDG_CONFIG_HOME}/java
-HISTFILE        DEFAULT=${XDG_STATE_HOME}/zsh/history
-
-GTK_IM_MODULE   DEFAULT=fcitx
-QT_IM_MODULE    DEFAULT=fcitx
-XMODIFIERS      DEFAULT=\@im=fcitx
-INPUT_METHOD    DEFAULT=fcitx
-SDL_IM_MODULE   DEFAULT=fcitx
-
-EDITOR          DEFAULT=nano
-VISUAL          DEFAULT=nano
-SUDO_EDITOR     DEFAULT=nano' > ${HOME}/.pam_environment
+cp ../.config $HOME/ -r
+cp ../.pam_environment $HOME/ -r
+# This will be deleted in the future and will be replaced by file copying
 echo '# Setting zsh cache.
 ZSH_CACHE_DIR=${HOME}/.cache/zsh
 if [[ ! -d ${ZSH_CACHE_DIR} ]]; then
@@ -76,7 +60,7 @@ DISABLE_AUTO_UPDATE="true"
 ZSH_THEME="gnzh"
 plugins=(sudo zsh-syntax-highlighting zsh-autosuggestions vscode)
 source ${ZSH}/oh-my-zsh.sh' > ${HOME}/.zshrc
-export GNUPGHOME="$XDG_DATA_HOME"/gnupg # Prevent the unavailability of environment variables caused by not restarting the terminal
+# Prevent the unavailability of environment variables caused by not restarting the terminal
+export GNUPGHOME="$XDG_DATA_HOME"/gnupg
 rm ${HOME}/.gnupg
-cp ../.config $HOME/ -r
 chsh -s /bin/zsh
