6fffc95b071725b2ac73184140795d51512f5f5a
Star_caorui <star_caorui@hotmail.com>
2022-02-14 11:10:21 +0800

    Update private_dot_config/zsh/dot_zshrc.tmpl Update private_dot_local/bin/auto-conf-arch.sh

--- private_dot_local/bin/auto-conf-arch.sh
+++ private_dot_local/bin/auto-conf-arch.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/bash
 # File: auto_conf_arch.sh
 # Author: Star_caorui
-# Last modified: UTC+8 2021-12-13 10:07
+# Last modified: UTC+8 2021-2-14 3:03
 
 # HACK: Maybe you need install ucode, firmware, graphics driver.
+# HACK: Maybe you need install App: axel zip unzip neofetch v2ray php php-fpm php-sqlite nginx-mainline certbot-nginx goaccess
+# TODO: ip(6)table: Tencent Cloud SDK
 
 # 偏好设置：
 # _ROOT_PASSWORD=  # 如果启用将会给 root 用户创建账户密码
-# _PASSWORD_LOGIN= # 如果值是 ENABLE 则允许密码登入，如果值是 DISABLE 则不允许密码登入。
 
-_PASSWORD_LOGIN=DISABLE
 # _ROOT_PASSWORD=
 _TIMEZONE=Asia/Shanghai
 _LANG=zh_CN.UTF-8
-_SHELL=zsh
 _EDITOR=nano
 #_HOSTNAME=iNetech XXX
 #_DNS_OVER_HTTPS='https://i.passcloud.xyz/dns-query'
 
 if(`curl -s "https://api.ip.sb/geoip" | grep -oP '(?<=country_code":")\w+'` = 'CN') {
   _DISABLE_CLOUDFALRE_FOR_CN='#'
+}
+
+function auto_conf_vps2arch() {
+  pacman --noconfirm -S linux-zen
+  systemctl disable --now reflector
+  pacman --noconfirm -Rsn reflector lvm2 linux
+  grub-mkconfig -o /boot/grub/grub.cfg
 }
 
 function auto_conf_locale() {
@@ -37,6 +43,11 @@ function auto_conf_locale() {
 }
 
 function auto_conf_env() {
+  # TODO global env setting
+  echo 'http ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+  # TODO: Install paru.
+  sudo -u http paru --noconfirm -S chezmoi sudo zsh zinit-git oh-my-zsh-git zsh-autosuggestions zsh-completions zsh-syntax-highlighting htop git gpg ${_EDITOR}
+
   mkdir -p ${HOME}/.local/state
   mkdir -p ${HOME}/.config/chezmoi
 
@@ -50,11 +61,6 @@ function auto_conf_env() {
     email = "star_caorui@hotmail.com"
 	EOF
 
-  echo 'http ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
-
-  pacman --noconfirm -S chezmoi sudo zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting
-  # TODO: Install paru.
-  sudo -u http paru --noconfirm -S zinit-git oh-my-zsh-git
   chezmoi init --apply https://github.com/Star-caorui/Star-caorui/
   chsh -s /bin/zsh
   # Prevent the unavailability of environment variables caused by not restarting the terminal
@@ -62,6 +68,27 @@ function auto_conf_env() {
   if [[ -d ${HOME}/.gnupg ]]; then
     rm ${HOME}/.gnupg
   fi
+}
+
+function auto_conf_iptable() {
+  iptables  -F
+  ip6tables -F
+
+  iptables  -P INPUT DROP
+  ip6tables -P INPUT DROP
+  iptables  -P FORWARD DROP
+  ip6tables -P FORWARD DROP
+  iptables  -P OUTPUT ACCEPT
+  ip6tables -P OUTPUT ACCEPT
+
+  iptables  -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+  ip6tables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+  iptables-save  > /etc/iptables/iptables.rules
+  ip6tables-save > /etc/iptables/ip6tables.rules
+
+  systemctl enable iptables
+  systemctl enable ip6tables
 }
 
 function auto_conf_doh() {
@@ -111,30 +138,9 @@ function auto_conf_doh() {
   fi
 }
 
-function auto_conf_iptable() {
-  iptables  -F
-  ip6tables -F
-
-  iptables  -P INPUT DROP
-  ip6tables -P INPUT DROP
-  iptables  -P FORWARD DROP
-  ip6tables -P FORWARD DROP
-  iptables  -P OUTPUT ACCEPT
-  ip6tables -P OUTPUT ACCEPT
-
-  iptables  -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-  ip6tables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-
-  iptables-save  > /etc/iptables/iptables.rules
-  ip6tables-save > /etc/iptables/ip6tables.rules
-
-  systemctl enable iptables
-  systemctl enable ip6tables
-}
-
 function auto_conf_pacman() {
     cat > /etc/pacman.d/mirrorlist <<-EOF
-      Server = https://cloudflaremirrors.com/archlinux/\$repo/os/\$arch
+      Server = ${_DISABLE_CLOUDFALRE_FOR_CN}https://cloudflaremirrors.com/archlinux/\$repo/os/\$arch
       Server = https://mirrors.bfsu.edu.cn/archlinux/\$repo/os/\$arch
       Server = https://mirrors.neusoft.edu.cn/archlinux/\$repo/os/\$arch
       Server = https://mirrors.tuna.tsinghua.edu.cn/archlinux/\$repo/os/\$arch
@@ -149,26 +155,6 @@ function auto_conf_pacman() {
     sed -i 's/#Color/Color/' /etc/pacman.conf
     sed -i 's/#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf
     sed -i 's/#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
-
-    if [[ -f '/version' ]]; then
-      pacman-key --init && pacman-key --populate archlinux
-    else
-      cat >> /etc/pacman.conf <<-EOF
-        [archlinuxcn]
-        Include = /etc/pacman.d/mirrorlist-cn
-
-        #[zhullyb]
-        #Server = https://pkg.web-worker.cn/zhullyb
-
-        #[web-worker]
-        #Server = https://pkg.web-worker.cn/archlinux
-			EOF
-      pacman --noconfirm -Sy archlinuxcn-keyring
-    fi
-    #sed -i 's/#\[zhullyb\]/\[zhullyb\]/' /etc/pacman.conf
-    #sed -i 's|#Server = https://pkg.web-worker.cn/zhullyb|Server = https://pkg.web-worker.cn/zhullyb|' /etc/pacman.conf
-    #pacman --noconfirm -S zhullyb-keyring #web-worker-keyring
-    #pacman-key --lsign-key zhullyb
 }
 
 function auto_conf_sshd() {
@@ -177,22 +163,13 @@ function auto_conf_sshd() {
   iptables-save  > /etc/iptables/iptables.rules
   ip6tables-save > /etc/iptables/ip6tables.rules
 
-  if [[ ! -x `command -v ssh` ]]; then
-    pacman --noconfirm -S openssh
-  fi
-
   if [[ ${_ROOT_PASSWORD} ]]; then
     echo root:${_ROOT_PASSWORD}|chpasswd
   fi
 
-  if [[ ! -d ${HOME}/.ssh ]]; then
-    install -D -m 700 -o root -g root -d ${HOME}/.ssh
-  fi
-  echo ${_SSH_PUBKEY} > ${HOME}/.ssh/authorized_keys
-
   # Allow Pubkey Authentication
   sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config
-  if [[ ${_PASSWORD_LOGIN} = "ENABLE" ]]; then
+  if [[ ${_ROOT_PASSWORD} ]]; then
     sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
     sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
   else
@@ -210,22 +187,5 @@ function auto_conf_sshd() {
   sed -i 's/#X11UseLocalhost yes/X11UseLocalhost yes/' /etc/ssh/sshd_config
   echo 'StreamLocalBindUnlink yes' >> /etc/ssh/sshd_config
 
-  systemctl enable sshd
+  systemctl restart sshd
 }
-
-
-
-function auto_conf_utility_software() {
-  pacman --noconfirm -S htop git gpg ${_EDITOR}
-}
-
-function auto_conf_vps2arch() {
-  pacman --noconfirm -S linux-zen
-  systemctl disable --now reflector
-  pacman --noconfirm -Rsn reflector lvm2 linux
-  grub-mkconfig -o /boot/grub/grub.cfg
-}
-
-
-# HACK: Maybe you need install App: axel zip unzip neofetch v2ray php php-fpm php-sqlite nginx-mainline certbot-nginx goaccess
-# ip(6)table: Tencent Cloud SDK
